6bcf54b5ec7943d2c7ed9aed593bf7eb011e9587
Ryan Bazzana <ryan@lambagency.com.au>
2020-03-09 14:47:19 +1000

    Initial commit, class based components

---
+++ shopping-list/src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="meal-list"></div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+export type Meallist = Meal[];
+
+export type Meal = {
+  mealName: string;
+  ingredients: Ingredient[];
+};
+
+export type Ingredient = {
+  name: string;
+  amount: string;
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.meal-list {
+  width: 50%;
+  height: 100%;
+  display: inline-block;
+}
+</style>
